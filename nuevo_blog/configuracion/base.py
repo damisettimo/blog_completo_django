58ee5fb7bd6d6e05a0a666bb1a2a3c4291b244cd
damisettimo <damisettimo@gmail.com>
2022-06-06 21:29:43 -0300

    Update base.py

--- nuevo_blog/configuracion/base.py
+++ nuevo_blog/configuracion/base.py
@@ -18,8 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'damisettimo@gmail.com'
+EMAIL_HOST_PASSWORD = 'coderhouse2022'
 EMAIL_PORT = 587
 
 # Quick-start development settings - unsuitable for production
